eabdd19435d1c7557944d22ad55b737f50b37019
Unknown <grace.kan@education.nsw.gov.au>
2018-06-28 15:01:25 +1000

    changed visuals

--- checkersgame.py
+++ checkersgame.py
@@ -72,6 +72,8 @@ while True:
         print('Error - try again')
         continue
 
+    list[yPossibleCoord][xPossibleCoord] = "WO"
+    list[y1Coord][x1Coord] = None
 
     print('Moved', coordIn,'to', coordOut)
     #insert AI's move
