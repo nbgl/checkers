981b4ea1dfc398af27e648d0e260c0999edfb8bd
Unknown <grace.kan@education.nsw.gov.au>
2018-06-28 14:20:00 +1000

    changed input

--- checkersgame.py
+++ checkersgame.py
@@ -22,45 +22,40 @@ def print_board(board):
                 row[index] = '--'
         print (" ".join(row))
 
-print_board(list)
 
 #def process_input():
 
 while True:
+    print_board(list)
     coordIn = input('Enter the coordinates of which piece you would like to move:')
     #check if it is possible to move it first - not surrounded / blocked by other pieces
-    x1Coord = coordIn[0]
-    y1Coord = coordIn[-1]
+    x1Coord = int(coordIn[0])
+    y1Coord = int(coordIn[-1])
     print (x1Coord)
     print(y1Coord)
     if list[y1Coord][x1Coord] != None and list[y1Coord][x1Coord] != 'BO':
-        if list[y1Coord-1][x1Coord-1] = None:
+        if list[y1Coord-1][x1Coord-1] == None:
             list.append([list[y1Coord-1][x1Coord-1]])
-        elif list[y1Coord-1][x1Coord+1] = None:
+        elif list[y1Coord-1][x1Coord+1] == None:
             list.append([list[y1Coord-1][x1Coord+1]])
-        elif list[y1Coord+1][x1Coord-1] = None:
+        elif list[y1Coord+1][x1Coord-1] == None:
             list.append([list[y1Coord+1][x1Coord-1]])
-        elif list[y1Coord+1][x1Coord+1] = None:
+        elif list[y1Coord+1][x1Coord+1] == None:
             list.append([list[y1Coord+1][x1Coord+1]])
-        else:
-            print('Error - try again')
-            continue
-    #remember
-    if [x1Coord] = 7
+    else:
+        print('Error - try again')
+        continue
+    #if [x1Coord] = 7
+    #board[BoardIndex][ListIndex]
+    coordOut = input('Enter where you would like to move it: ')
+    x2Coord = int(coordIn[0])
+    y2Coord = int(coordIn[-1])
+    print (x2Coord)
+    print(y2Coord)
+    #check if it is possible to move it to that area
+    print('Moved', coordIn,'to', coordOut)
+    #insert AI's move
 
-
-    if
-
-
-
-#board[BoardIndex][ListIndex]
-coordOut = input('Enter where you would like to move it: ')
-x2Coord = coordIn[0]
-y2Coord = coordIn[-1]
-print (x2Coord)
-print(y2Coord)
-#check if it is possible to move it to that area
-print('Moved', coordIn,'to', coordOut)
 
 
 
